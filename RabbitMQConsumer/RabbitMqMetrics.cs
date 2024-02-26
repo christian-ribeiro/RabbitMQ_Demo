0efc70566a8a55672fe2108a83e0934a44bccf7f
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-02-26 12:41:33 -0300

    Test - Metric API - Rabbit

---
+++ RabbitMQConsumer/RabbitMqMetrics.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RabbitMQConsumer;
+
+public class RabbitMqMetrics
+{
+    private readonly Uri _baseUri = new Uri("http://localhost:15672/api/");
+    private readonly string _username = "guest";
+    private readonly string _password = "guest";
+    private readonly HttpClient _httpClient;
+
+    public RabbitMqMetrics()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = _baseUri;
+        var authToken = Encoding.ASCII.GetBytes($"{_username}:{_password}");
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+    }
+
+    public async Task<(int Consumers, int Action)> GetQueueUsagePercentageAsync()
+    {
+        string queueName = "queue_demo";
+        string vhost = Uri.EscapeDataString("/");
+        HttpResponseMessage response = await _httpClient.GetAsync($"queues/{vhost}/{queueName}");
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var queueDetails = JsonSerializer.Deserialize<Metrics>(content, options);
+
+            if (queueDetails == null)
+                return (0, 0);
+
+            var consumerUtilisation = queueDetails.ConsumerUtilisation * 100;
+            var consumers = queueDetails.Consumers;
+
+            Console.WriteLine($"Utilisation: {consumerUtilisation}");
+            Console.WriteLine($"Consumers: {consumers}");
+
+            if (consumerUtilisation > 50 || consumers == 0)
+                return (consumers, 1);
+            else if (consumerUtilisation < 20 && consumers > 1)
+                return (consumers, -1);
+        }
+        else
+        {
+            Console.WriteLine($"Failed to retrieve queue details. Status code: {response.StatusCode}");
+        }
+
+        return (0, 0);
+    }
+
+    public class Metrics
+    {
+        [JsonPropertyName("consumer_capacity")]
+        public double ConsumerCapacity { get; set; }
+        [JsonPropertyName("consumer_utilisation")]
+        public double ConsumerUtilisation { get; set; }
+        [JsonPropertyName("consumers")]
+        public int Consumers { get; set; }
+    }
+
+}
